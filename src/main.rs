2d2dfb9d328a0279ed1f8d91db22249527ebaf6e
Dominik Boehler <dominik.boehler@gmx.net>
2024-05-07 05:46:35 +0200

    solution

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use simple_linked_list::SimpleLinkedList;
+
+fn main() -> () {
+    // let mut list = SimpleLinkedList::new();
+    // list.push("3");
+    // list.push("2");
+    // list.push("1");
+    // println!("{}",reversed.pop().unwrap());
+    // println!("{}",reversed.pop().unwrap());
+    // println!("{}",reversed.pop().unwrap());
+}
